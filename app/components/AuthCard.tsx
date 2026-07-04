40759b138403ad43ee20ebd7eea3bdf9d3d368bf
Arpit Kansal <155746706+ArpitKansal01@users.noreply.github.com>
2026-07-05 03:33:58 +0530

    Update Google auth API endpoint to production URL

--- app/components/AuthCard.tsx
+++ app/components/AuthCard.tsx
@@ -16,7 +16,7 @@ export default function AuthCard({ signup }: Props) {
     flow: "auth-code",
 
     onSuccess: async (response) => {
-      const res = await fetch("http://localhost:5000/api/auth/google", {
+      const res = await fetch("https://google-cloud-auth-my2c.vercel.app/api/auth/google", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
